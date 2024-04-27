d6bd012a3a6a23b955374a381ce8c5ec13c6fd09
syedfarhanhabib <syedfarhandesigns@gmail.com>
2024-04-27 12:02:17 +0500

    Second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { spaceGrotesk } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${spaceGrotesk.className} antialiased`}>{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Space_Grotesk} from 'next/font/google';
+ 
+export const inter = Inter({
+     subsets: ['latin'],
+     display: 'swap',
+});
+
+export const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  display: 'swap',
+})
