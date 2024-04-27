8c822574f669ee34b6a489ded4a12e2ea0168238
syedfarhanhabib <syedfarhandesigns@gmail.com>
2024-04-27 12:17:44 +0500

    handling errors for deployment

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
-import { Inter, Space_Grotesk} from 'next/font/google';
+import { Lusitana, Space_Grotesk} from 'next/font/google';
  
-export const inter = Inter({
-     subsets: ['latin'],
-     display: 'swap',
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
 });
 
 export const spaceGrotesk = Space_Grotesk({
